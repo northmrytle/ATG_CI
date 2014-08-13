6be32f25faf2d9d239057b13294eb5db377c0481
northmyrtle <todd@northmyrtle.net>
2014-08-13 00:09:57 -0500

    added form

--- js/index.js
+++ js/index.js
@@ -62,16 +62,18 @@ var app = {
 
         scanner.scan( function (result) { 
 
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
+//            alert("We got a barcode\n" + 
+//            "Result: " + result.text + "\n" + 
+//            "Format: " + result.format + "\n" + 
+//            "Cancelled: " + result.cancelled);  
 
            console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
+            document.getElementById("scan").value=result.text;
+            
             console.log(result);
             /*
             if (args.format == "QR_CODE") {
