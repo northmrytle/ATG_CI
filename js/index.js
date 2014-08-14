9fc301854290de0fc722553492995f13c5570404
northmyrtle <todd@northmyrtle.net>
2014-08-14 15:50:54 -0500

    29

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+    
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -12,7 +13,9 @@ var app = {
         document.getElementById('scan').addEventListener('click', this.scan, false);
         document.getElementById('settings').addEventListener('click', this.settings, false);
         document.getElementById('settings_close').addEventListener('click', this.settings_close, false);
+        document.getElementById('btnNext').addEventListener('click', this.openWebApp, false);
     },
+    
     // deviceready Event Handler
     //
     // The scope of `this` is the event. In order to call the `receivedEvent`
@@ -20,6 +23,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
+    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -32,12 +36,17 @@ var app = {
         console.log('Received Event: ' + id);
     },
     
+    openWebApp: function(){
+        var scanText = document.getElementById("scantext").value;
+        var webAppUrl = "http://192.168.2.235/ATG_CI/Checkin.aspx?scan=" + scanText;
+        var ref = window.open(webAppUrl, '_blank', 'location=yes');
+        
+    },
+            
     settings: function() {
         document.getElementById("light").setAttribute('style', 'display:block');
         document.getElementById("fade").setAttribute('style', 'display:block');
-        
-        alert("getting settings");
-        
+                
 //        var webAppUrl = window.applicationPreferences.get(
 //                "webAppUrl", 
 //                function(value) {alert("Value is " + value);}, 
@@ -46,12 +55,9 @@ var app = {
     },
 
     settings_close: function(){
-//        alert("settings_save");
         
         var webAppUrl = document.getElementById("webAppUrl").value;
         
-        alert("saving " + webAppUrl);
-
 //        window.applicationPreferences.set("webAppUrl", webAppUrl, 
 //                function() {alert("Successfully saved!");},
 //                function(error) {alert("Error! " + JSON.stringify(error));}
@@ -59,21 +65,9 @@ var app = {
 //               
         document.getElementById("light").setAttribute('style', 'display:none');
         document.getElementById("fade").setAttribute('style', 'display:none');
+        
     },
-        
-    settings_save: function() {
-    
-        alert("settings_save");
-        
-//        var webAppUrl = document.getElementById("webappurl").value;
-//        alert("saving " + webAppUrl);
-        
-//        window.applicationPreferences.set("webAppUrl", webAppUrl, 
-//            function() {alert("Successfully saved!");},
-//            function(error) {alert("Error! " + JSON.stringify(error));}
-//            );
-    },      
-    
+      
     scan: function() {
         console.log('scanning');
 
@@ -86,18 +80,10 @@ var app = {
 //            "Format: " + result.format + "\n" + 
 //            "Cancelled: " + result.cancelled);  
 
-            console.log("Scanner result: \n" +
-                    "text: " + result.text + "\n" +
-                    "format: " + result.format + "\n" +
-                    "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
-
 //            document.getElementById("scan").value = "1GNFK13509R163698";
 
-            console.log(result);
-
-//            var ref = window.open('http://www.google.com?q=' + result.text, '_blank', 'location=yes');
-//            var ref = window.open('http://192.168.2.235/atg_ci/checkin.aspx?scan=' + result.text, '_blank', 'location=yes');
+            var ref = window.open('http://192.168.2.235/ATG_CI/Checkin.aspx?scan=' + result.text, '_blank', 'location=yes');
 
         }, function(error) {
             console.log("Scanning failed: ", error);
